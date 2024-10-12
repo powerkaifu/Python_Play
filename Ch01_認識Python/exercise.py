0de340a6281f766cf7c33ff58844ebc6ebc8dcb1
KaiFu <powerkaifu@gmail.com>
2024-10-12 17:38:34 +0800

    Ch01 習題練習

---
+++ Ch01_認識Python/exercise.py
@@ -0,0 +1,14 @@
+# (a) 計算 1 + 3 + 5
+print(1 + 3 + 5)
+
+# (b) 印出 'Hello, World!'
+print('Hello, World!')
+
+# (c) 計算 28 x 16 的值
+print(28 * 16)
+
+# (d) 計算 3 + 2**3 * (4+3)
+print(3 + 2**3 * (4 + 3))
+
+# (e) 計算 1~15 的總和
+print(sum(range(1, 16)))
